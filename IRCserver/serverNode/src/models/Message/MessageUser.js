46a3c60bd6c635561116b8bdb12cfa66a9a8939c
geoffroy <geoffroy.huck@epitech.eu>
2020-04-02 19:29:23 +0200

    add server working safe

--- IRCserver/serverNode/src/models/Message/MessageUser.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const MessageUser = sequelize.define('MessageUser', {})
-
-  MessageUser.associate = function (models) {
-    MessageUser.belongsTo(models.User)
-    MessageUser.belongsTo(models.Message)
-  }
-  return MessageUser
-}
--- IRCserver/serverNode/src/models/Message/RespMessage.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const RespMessage = sequelize.define('RespMessage', {})
-
-  RespMessage.associate = function (models) {
-    RespMessage.belongsTo(models.Resp)
-    RespMessage.belongsTo(models.Message)
-  }
-  return RespMessage
-}
