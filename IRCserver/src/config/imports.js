86c8fff7a4838e31447310e89b9e3cf52757b6ea
Volkramer <volkramer@gmail.com>
2020-04-03 03:18:00 +0200

    add socket.io into import

--- IRCserver/src/config/imports.js
+++ IRCserver/src/config/imports.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const bodyParser = require('body-parser') // process json data
-const cors = require('cors')
-const morgan = require('morgan')
-const config = require('./config')
-const fs = require('fs')
+const express = require("express");
+const bodyParser = require("body-parser"); // process json data
+const cors = require("cors");
+const morgan = require("morgan");
+const config = require("./config");
+const fs = require("fs");
+const socketio = require("socket.io");
 
 const imports = {
-    express: express,
-    bodyParser: bodyParser,
-    cors: cors,
-    morgan: morgan,
-    config: config,
-    fs: fs
-}
+  express: express,
+  bodyParser: bodyParser,
+  cors: cors,
+  morgan: morgan,
+  config: config,
+  fs: fs,
+  socketio: socketio
+};
 
-module.exports = imports
+module.exports = imports;
--- IRCserver/src/socket/chat-server.js
+++ IRCserver/src/socket/chat-server.js
@@ -1,57 +1,58 @@
-var io = require('socket.io')(http);
-var Channel = require('./channels.js');
-var User = require('./users.js');
+var Channel = require("./objects/channels");
+var User = require("./objects/users.js");
 
-var users = [];
-var channels = [];
+module.exports = function(io) {
+  var users = [];
+  var channels = [];
 
-io.on('connection', function(socket) {
-  var user = new User(socket, 'anonymous');
-  var address = user.socket.request.connection.remoteAddress;
-  var socketID = user.socket.id;
+  io.on("connection", function(socket) {
+    var user = new User(socket, "anonymous");
+    var address = user.socket.request.connection.remoteAddress;
+    var socketID = user.socket.id;
 
-  users.push(user);
-  console.log('user ' + address + ' connected. Socket ID: ' + socketID);
+    users.push(user);
+    console.log("user " + address + " connected. Socket ID: " + socketID);
 
-  socket.on('nickname', function(nickname) {
-    for (let i = 0; i < users.length; i++) {
-      if (users[i].socket.id === socket.id) {
-        users[i].nickname = nickname;
-        user.nickname = nickname;
-        console.log(
-          'user ' + user.socket.id + 'change his nickname for ' + nickname
-        );
+    socket.on("nickname", function(nickname) {
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].socket.id === socket.id) {
+          users[i].nickname = nickname;
+          user.nickname = nickname;
+          console.log(
+            "user " + user.socket.id + "change his nickname for " + nickname
+          );
+        }
       }
-    }
-  });
+    });
 
-  socket.on('join-channel', function(channelName) {
-    for (let i = 0; i < channels.length; i++) {
-      if (channels[i].name === channelName) {
-        channels[i].addUser(user);
-        console.log(channels);
-        console.log('\n');
-        return;
+    socket.on("join-channel", function(channelName) {
+      for (let i = 0; i < channels.length; i++) {
+        if (channels[i].name === channelName) {
+          channels[i].addUser(user);
+          console.log(channels);
+          console.log("\n");
+          return;
+        }
       }
-    }
-    channel = new Channel(channelName, user);
-    channels.push(channel);
-    channel.addUser(user);
-    console.log(channels);
-    console.log('\n');
-  });
+      channel = new Channel(channelName, user);
+      channels.push(channel);
+      channel.addUser(user);
+      console.log(channels);
+      console.log("\n");
+    });
 
-  socket.on('disconnect', function() {
-    for (let i = 0; i < users.length; i++) {
-      if (users[i].socket.id === socket.id) {
-        users.splice(i, 1);
+    socket.on("disconnect", function() {
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].socket.id === socket.id) {
+          users.splice(i, 1);
+        }
       }
-    }
-    console.log('user ' + socketID + ' disconnected.');
-  });
+      console.log("user " + socketID + " disconnected.");
+    });
 
-  socket.on('new-message', message => {
-    io.emit('new-message', message);
-    console.log(message);
+    socket.on("new-message", message => {
+      io.emit("new-message", message);
+      console.log(message);
+    });
   });
-});
+};
